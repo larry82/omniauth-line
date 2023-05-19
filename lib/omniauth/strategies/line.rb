1d15038fb735d5fcd45fcbd0e795563a161bb4ae
Wayne Chen <xwaynec@gmail.com>
2023-05-19 17:22:37 +0800

    refine line strategy

--- lib/omniauth/strategies/line.rb
+++ lib/omniauth/strategies/line.rb
@@ -19,6 +19,19 @@ module OmniAuth
         super
       end
 
+      # Correct redirect_uri_mismatch issues related to callback_uri through path match.
+      # NOTE If we're using code from the signed request then FB sets the redirect_uri to '' during the authorize
+      #      phase and it must match during the access_token phase:
+      #      https://github.com/facebook/facebook-php-sdk/blob/master/src/base_facebook.php#L477
+      def callback_url
+        if @authorization_code_from_signed_request_in_cookie
+          ''
+        else
+          # callback url ignorance issue from https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+          options[:callback_url] || (full_host + script_name + callback_path)
+        end
+      end
+
       uid { raw_info['userId'] }
 
       info do
@@ -26,20 +39,35 @@ module OmniAuth
           name:        raw_info['displayName'],
           image:       raw_info['pictureUrl'],
           description: raw_info['statusMessage'],
-          email:       JWT.decode(access_token.params['id_token'], ENV['LINE_SECRET']).first['email']
+          email: email
         }
       end
 
       # Require: Access token with PROFILE permission issued.
       def raw_info
+        @id_token = access_token.params["id_token"]
         @raw_info ||= JSON.load(access_token.get('v2/profile').body)
       rescue ::Errno::ETIMEDOUT
         raise ::Timeout::Error
       end
 
-      def callback_url
-        # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
-        options[:callback_url] || (full_host + script_name + callback_path)
+      def email
+        uri = URI.parse("https://api.line.me/oauth2/v2.1/verify")
+        request = Net::HTTP::Post.new(uri)
+        request.set_form_data(
+          "client_id" => client.id,
+          "id_token" => @id_token
+        )
+
+        req_options = {
+          use_ssl: uri.scheme == "https",
+        }
+
+        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+          http.request(request)
+        end
+
+        JSON.parse(response.body)["email"]
       end
     end
   end
